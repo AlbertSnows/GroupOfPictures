bed7dbce8915c270f91fde9e6309399e55d99988
Albert Snow <ajsnow2012@gmail.com>
2023-12-23 00:25:28 -0600

    i got start and end for endpoint 2

--- src/main/java/ajsnow/playground/groupofpictures/controllers/VideoController.java
+++ src/main/java/ajsnow/playground/groupofpictures/controllers/VideoController.java
@@ -3,10 +3,19 @@ package ajsnow.playground.groupofpictures.controllers;
 import ajsnow.playground.groupofpictures.services.video.VideoRead;
 import com.github.kokorin.jaffree.ffprobe.data.ProbeData;
 import com.grack.nanojson.JsonArray;
+import com.grack.nanojson.JsonObject;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/videos")
@@ -20,28 +29,61 @@ public class VideoController {
     }
 
     @GetMapping("/{videoName}")
-    public ResponseEntity<JsonArray> recordReceipt(@PathVariable("videoName") String name) {
+    public ResponseEntity<JsonArray> getFrameData(@PathVariable("videoName") String name) {
         // cases
         // file not found 404
         // file not parsable
         // file parsable, give data
-        var x = videoRead.video.videoProbe
+        // remove cast
+        var frameData = videoRead.video.videoProbe
                 .setShowFrames(true)
                 .execute();
-        return ResponseEntity.ok((JsonArray) x.getData().getValue("frames"));
+        return ResponseEntity.ok((JsonArray) frameData.getData().getValue("frames"));
     }
 
 
     @GetMapping("/{videoName}/group-of-pictures/{groupIndex}")
-    public HttpStatusCode listAllData(@PathVariable("videoName") String name,
-                                      @PathVariable("groupIndex") String index) {
+    public HttpStatusCode streamGroupVideo(@PathVariable("videoName") String name,
+                                      @PathVariable("groupIndex") @NotNull String indexName) {
+        var dataForFrames = videoRead.video.videoProbe
+                .setShowFrames(true)
+                .execute();
+        JsonArray frameList = (JsonArray) dataForFrames.getData().getValue("frames");
+//        Stream<JsonObject> frameStream = IntStream.range(0, frameList.size()).mapToObj(frameList::getObject);
+        IntPredicate isIFrame = frameIndex -> {
+            var frameData = (JsonObject) frameList.get(frameIndex);
+            return frameData.get("pict_type").equals("I");
+        };
+        var iFrameIndexes = IntStream
+                .rangeClosed(0, frameList.size()-1)
+                .filter(isIFrame)
+                .boxed()
+                .collect(Collectors.toCollection(TreeSet::new));
+        var start = Integer.parseInt(indexName.split("\\.mp4")[0]);
+        var nextIFrame = iFrameIndexes.higher(start);
+        var end = nextIFrame - 1;
+
+        var x = new TreeSet<>(iFrameIndexes)
+//                .toArray()
+//                .collect(HashSet::new)
+                ;
+        // frame id -> data
+        // ask for I frame 87
+        // what frame do I stop just before?
+        //
+
+
+//                frameStream.filter(isIFrame).toList();
+//        var startFrame = iFrameIndexes.toArray()[)];
+//        var videoSnippet = videoRead.video.videoData
+//                .
         // fnf, fnp, index not found, index not parsable
         // give data
         return HttpStatusCode.valueOf(501);
     }
 
     @GetMapping("/{videoName}/group-of-pictures")
-    public HttpStatusCode getPoints(@PathVariable("videoName") String name) {
+    public HttpStatusCode getFramesAsVideos(@PathVariable("videoName") String name) {
         // fnf, fnp
         // indexes not possible { list ...}
         // return html doc
--- src/main/java/ajsnow/playground/groupofpictures/data/Video.java
+++ src/main/java/ajsnow/playground/groupofpictures/data/Video.java
@@ -1,5 +1,8 @@
 package ajsnow.playground.groupofpictures.data;
 
+import com.github.kokorin.jaffree.ffmpeg.BaseInput;
+import com.github.kokorin.jaffree.ffmpeg.FFmpeg;
+import com.github.kokorin.jaffree.ffmpeg.UrlInput;
 import com.github.kokorin.jaffree.ffprobe.FFprobe;
 import com.github.kokorin.jaffree.ffprobe.FFprobeResult;
 import org.springframework.stereotype.Component;
@@ -11,10 +14,13 @@ import java.util.function.Consumer;
 @Component
 public class Video {
     public final FFprobe videoProbe;
+    public final FFmpeg videoData;
     public Video() {
         var pathToVideo = Paths.get("src/main/resources/source/CoolVideo.mp4");
         videoProbe = FFprobe.atPath()
                 .setInput(pathToVideo);
+        videoData = FFmpeg.atPath()
+                .addInput(UrlInput.fromPath(pathToVideo));
 //        System.out.println("Video ingested...");
     }
 }
