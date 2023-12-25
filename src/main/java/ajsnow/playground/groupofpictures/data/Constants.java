56bc2755dfbc061dc989a41adf6e101c9dae4565
Albert Snow <ajsnow2012@gmail.com>
2023-12-25 15:25:15 -0600

    small fix

--- src/main/java/ajsnow/playground/groupofpictures/data/Constants.java
+++ src/main/java/ajsnow/playground/groupofpictures/data/Constants.java
@@ -7,6 +7,6 @@ public class Constants {
     public static final String STATIC_PATH = "src/main/resources/static/";
     public static @NotNull String getHTMLPath() {
         String os = System.getProperty("os.name").toLowerCase();
-        return os.contains("win") ? STATIC_PATH : "app/static";
+        return os.contains("win") ? STATIC_PATH : "app/static/";
     }
 }
