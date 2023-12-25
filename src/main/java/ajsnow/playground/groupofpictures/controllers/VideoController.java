2e0c044f306669cdf97e0a215b20e9defd573efe
Albert Snow <ajsnow2012@gmail.com>
2023-12-24 18:25:54 -0600

    ep2 has been rop'ed

--- src/main/java/ajsnow/playground/groupofpictures/controllers/VideoController.java
+++ src/main/java/ajsnow/playground/groupofpictures/controllers/VideoController.java
@@ -2,23 +2,22 @@ package ajsnow.playground.groupofpictures.controllers;
 
 import ajsnow.playground.groupofpictures.services.routing.RoutingCore;
 import ajsnow.playground.groupofpictures.services.video.VideoRead;
+import ajsnow.playground.groupofpictures.services.video.VideoWrite;
+import ajsnow.playground.groupofpictures.utility.rop.result.Result;
 import ajsnow.playground.groupofpictures.utility.rop.result.TypeOf;
 import com.github.kokorin.jaffree.ffmpeg.FFmpeg;
 import com.github.kokorin.jaffree.ffmpeg.UrlInput;
 import com.github.kokorin.jaffree.ffmpeg.UrlOutput;
-import com.github.kokorin.jaffree.ffprobe.FFprobe;
 import com.grack.nanojson.JsonArray;
 import com.grack.nanojson.JsonObject;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -29,7 +28,9 @@ import java.util.stream.IntStream;
 import org.apache.commons.text.StringEscapeUtils;
 
 import static ajsnow.playground.groupofpictures.data.Constants.SOURCE_PATH;
+import static ajsnow.playground.groupofpictures.utility.rop.functions.HigherOrderFunctions.upcast;
 import static ajsnow.playground.groupofpictures.utility.rop.result.Resolvers.collapse;
+import static ajsnow.playground.groupofpictures.utility.rop.result.Resolvers.collapseToBoolean;
 import static ajsnow.playground.groupofpictures.utility.rop.result.Transformers.*;
 import static ajsnow.playground.groupofpictures.utility.rop.result.TypeOf.using;
 import static ajsnow.playground.groupofpictures.utility.rop.wrappers.Piper.pipe;
@@ -65,92 +66,20 @@ public class VideoController {
     public ResponseEntity<?> streamGroupVideo(@PathVariable("videoName") String name,
                                               @PathVariable("groupIndex") @NotNull String indexName) {
         var escapedName = StringEscapeUtils.escapeJava(name);
-        var videoDirectory = "src/main/resources/source/";
-        var path = Path.of(videoDirectory + escapedName);
-        var fileExists = Files.exists(path);
-        var videoProbe = FFprobe.atPath().setInput(path);
-        if(!fileExists) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("File not found!");
-        }
-        var dataForFrames = videoProbe.setShowFrames(true).execute();
-        JsonArray frameList = (JsonArray) dataForFrames.getData().getValue("frames");
-        IntPredicate isIFrame = frameIndex -> {
-            var frameData = (JsonObject) frameList.get(frameIndex);
-            return frameData.get("pict_type").equals("I");
-        };
-        var iFrameIndexes = IntStream
-                .rangeClosed(0, frameList.size()-1)
-                .filter(isIFrame)
-                .boxed()
-                .collect(Collectors.toCollection(TreeSet::new));
-        var start = Integer.parseInt(indexName.split("\\.mp4")[0]);
-        var nextIFrame = iFrameIndexes.higher(start);
-        var pathToVideo = Paths.get("src/main/resources/source/" + name);
-        var videoData = FFmpeg.atPath().addInput(UrlInput.fromPath(pathToVideo));
         var escapedIndex = StringEscapeUtils.escapeJava(indexName);
-        var videoNameWithoutExtension = escapedName.split("\\.mp4")[0];
-        var clipDirectory = "src/main/resources/static/";
-        var newDirectory = clipDirectory + videoNameWithoutExtension;
-        try {
-            Files.createDirectories(Paths.get(newDirectory));
-        } catch (Exception ex) {
-            System.out.println("Problem creating directories! Error: " + ex.getMessage());
-            return ResponseEntity.internalServerError().body("Couldn't create a directory, sorry! :(");
-        }
-        var outputPath = Path.of(newDirectory + "/" + escapedIndex);
-        var urlOutput = UrlOutput.toPath(outputPath);
-        if(!iFrameIndexes.contains(start)) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Index not found!");
-        }
-        JsonObject startFrameData = (JsonObject) frameList.get(start);
-        if(nextIFrame == null) {
-            String startFrameRawTime = (String) startFrameData.get("pts_time");
-            var offset = 70;
-            var possibleStartTime = (1000 * Float.parseFloat(startFrameRawTime) - offset);
-            var startFrameTime = Math.max((int) possibleStartTime, 0) + "ms";
-            var extractGOP = videoData
-                    .addArguments("-ss", startFrameTime)
-                    .addOutput(urlOutput);
-            extractGOP.execute();
-        } else {
-            JsonObject endFrameData = (JsonObject) frameList.get(nextIFrame);
-            String startFrameRawTime = (String) startFrameData.get("pts_time");
-            var offset = 70;
-            var possibleStartTime = (1000 * Float.parseFloat(startFrameRawTime) - offset);
-            String endFrameRawTime = (String) endFrameData.get("pts_time");
-            var endTime = (int) (1000 * Float.parseFloat(endFrameRawTime));
-            var startFrameTime = Math.max((int) possibleStartTime, 0) + "ms";
-            var endFrameTime = endTime + "ms";
-            var extractGOP = videoData
-                    .addArguments("-ss", startFrameTime)
-                    .addArguments("-to", endFrameTime)
-                    .addArguments("-c:v", "copy")
-                    .addArguments("-c:a", "copy")
-                    .addOutput(urlOutput);
-            //Concern: doesn't work in debugger mode?
-            extractGOP.execute();
-        }
-        //Confusion: why doesn't this work???
-//        var clipFilePath = new ClassPathResource(
-//                String.format("static/" + videoNameWithoutExtension +"/" + escapedIndex),
-//                getClass().getClassLoader());
-
-        var clipFile = new File(String.format("src/main/resources/static/" + videoNameWithoutExtension +"/" + escapedIndex));
-        if(!clipFile.exists()) {
-            return ResponseEntity.internalServerError().body("Could not find the clip. " +
-                    "This is a really bizarre bug and I haven't been able to discern the cause. " +
-                    "Sorry about that! ");
-        }
-        try {
-            var videoBytes = Files.readAllBytes(clipFile.toPath());
-            return ResponseEntity.ok()
-                    .header("Content-Disposition", "attachment; filename=\""+escapedIndex+"\"")
-                    .body(new ByteArrayResource(videoBytes));
-        } catch (Exception ex) {
-            System.out.println("Problem sending response: " + ex.getMessage());
-        }
-        // give data
-        return ResponseEntity.internalServerError().body("Internal server error, everything is on fire AHHHHH!");
+        var sourceVideoLocation = Path.of(SOURCE_PATH + escapedName);
+        // bonus: write a function to generalize response entity types, so I don't have to upcaste
+        return VideoWrite
+                .tryClippingVideo(escapedName, escapedIndex, sourceVideoLocation)
+                .then(using(TypeOf.<ResponseEntity<?>>forFailures()))
+                .then(attempt(VideoRead::tryReadingVideo))
+                .then(onFailure(str -> ResponseEntity.ok().body(str)))
+                .then(using(TypeOf.<ResponseEntity<?>>forFailures()))
+                .then(onSuccess(byteList -> ResponseEntity.ok()
+                        .header("Content-Disposition", "attachment; filename=\""+escapedIndex+"\"")
+                        .body(byteList)))
+                .then(using(TypeOf.<ResponseEntity<?>>forSuccesses()))
+                .then(collapse());
     }
 
     // bonus: convert to rop
