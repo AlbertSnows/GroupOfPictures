e08bc480f52951322d45aa98191baaeb21d8c28c
Albert Snow <ajsnow2012@gmail.com>
2023-12-24 18:25:30 -0600

    adding a few small things

--- src/main/java/ajsnow/playground/groupofpictures/utility/GOPFileHelpers.java
+++ src/main/java/ajsnow/playground/groupofpictures/utility/GOPFileHelpers.java
@@ -1,15 +1,19 @@
 package ajsnow.playground.groupofpictures.utility;
 
+import ajsnow.playground.groupofpictures.utility.rop.result.Result;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Predicate;
 
-public class Files {
+import static ajsnow.playground.groupofpictures.data.Constants.STATIC_PATH;
+
+public class GOPFileHelpers {
     // not needed
     @Contract(pure = true)
     public static @NotNull Predicate<String> fileExists(String path) {
@@ -18,5 +22,14 @@ public class Files {
             var files = new HashSet<>(List.of(Objects.requireNonNull(folder.list())));
             return files.contains(name);
         };
+    }
+
+    public static @NotNull Result<String, String> handleCreatingDirectory(@NotNull String newDirectory) {
+        try {
+            java.nio.file.Files.createDirectories(Paths.get(newDirectory));
+            return Result.success("Directory created!");
+        } catch (Exception ex) {
+            return Result.failure("Couldn't create a directory! Error: " + ex.getMessage());
+        }
     }
 }
